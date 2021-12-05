24f8d94b2d7843fabca2bd02879f018c4b00945d
lgraul87 <lgraul87@gmail.com>
2021-12-05 15:32:11 +0100

    Added table funcionality

--- src/sales-history/dto/update-sales-history.dto.ts
+++ src/sales-history/dto/update-sales-history.dto.ts
@@ -1,12 +1,32 @@
+import { CreateBeerDto } from 'src/beers/dto/create-beer.dto';
+import { CreateBreakfastProductDto } from 'src/breakfast-products/dto/create-breakfast-product.dto';
 import { CreateCounterDto } from 'src/counter/dto/create-counter.dto';
+import { CreateDinnerProductDto } from 'src/dinner-products/dto/create-dinner-product.dto';
+import { CreateJuiceDto } from 'src/juices/dto/create-juice.dto';
+import { CreateLunchProductDto } from 'src/lunch-products/dto/create-lunch-product.dto';
+import { CreateSoftDrinkDto } from 'src/soft-drinks/dto/create-soft-drink.dto';
+import { CreateSpiritDrinkDto } from 'src/spirit-drinks/dto/create-spirit-drink.dto';
 import { CreateTableDto } from 'src/tables/dto/create-table.dto';
 import { CreateTakeAwayDto } from 'src/take-away/dto/create-take-away.dto';
 import { CreateWaiterDto } from 'src/waiters/dto/create-waiter.dto';
+import { CreateWineDto } from 'src/wines/dto/create-wine.dto';
 export class UpdateSalesHistoryDto {
+  beerProduct?: CreateBeerDto;
+  breakfastProduct?: CreateBreakfastProductDto;
+  dinnerProduct?: CreateDinnerProductDto;
+  juiceProduct?: CreateJuiceDto;
+  lunchProduct?: CreateLunchProductDto;
+  softDrinkProduct?: CreateSoftDrinkDto;
+  spiritDrinkProduct?: CreateSpiritDrinkDto;
+  wineProduct?: CreateWineDto;
+  units?: number;
+  liters?: number;
+  kilograms?: number;
+  price: number;
   table?: CreateTableDto;
   counter?: CreateCounterDto;
   takeAway?: CreateTakeAwayDto;
-  waiters: CreateWaiterDto;
-  date: Date;
-  bill: number;
+  waiters?: CreateWaiterDto;
+  date?: Date;
+  bill?: number;
 }
